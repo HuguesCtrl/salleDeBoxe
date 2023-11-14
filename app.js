a7152c80c6ad323be2abfbab9562e8547889f5c9
Hugues Collet <hc.pro@outlook.fr>
2023-11-14 18:03:59 +0100

    Fin section tarifs

---
+++ app.js
@@ -0,0 +1,35 @@
+//Récupère tous les boutons
+const allBtn = document.querySelectorAll(".btn-modale");
+//Selectionne la modale
+const modale = document.querySelector(".bloc-modale");
+//Selectionne le bouton de fermeture de la modale
+const closeModale = document.querySelector(".close-modale");
+//Récupère toutes les "images boutons"
+const imgIndex = document.querySelector(".bloc-modale img");
+//Responsive de la fenêtre
+const media1 = window.matchMedia("(min-width:850px)");
+
+if (media1.matches) {
+  allBtn.forEach((btn) =>
+    btn.addEventListener("click", (e) => {
+      imgIndex.src = `./ressources/img${e.target.getAttribute(
+        "data-index"
+      )}-equip.jpg`;
+      modale.classList.add("active-modale");
+    })
+  );
+
+  modale.addEventListener("click", () => {
+    modale.classList.remove("active-modale");
+  });
+}
+
+//Animation navbar
+const nav = document.querySelector("nav");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 30) {
+    nav.classList.add("anim-nav");
+  } else {
+    nav.classList.remove("anim-nav");
+  }
+});
